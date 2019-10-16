81c0e25568928bd849b71acd1c626fec88b793e8
DRandell <jup.trandell@gmail.com>
2019-10-16 20:46:52 +0100

    1.0.1

--- src/__tests__/JsonToCsvConversion.test.ts
+++ src/__tests__/JsonToCsvConversion.test.ts
@@ -4,27 +4,15 @@ const testJson = {
   lastName: 'Smith',
   title: 'Mr',
   department: 'HR',
-  job: 'HR Assistant'
+  job: 'HR Assistant',
 };
 
 test('Individual Object Conversion Column Names', () => {
   const jsonToCsvConversion = new JsonToCsvConversion(testJson);
-  expect(jsonToCsvConversion.columnNames).toStrictEqual([
-    'firstName',
-    'lastName',
-    'title',
-    'department',
-    'job'
-  ]);
+  expect(jsonToCsvConversion.columnNames).toStrictEqual(['firstName', 'lastName', 'title', 'department', 'job']);
 });
 
 test('Individual Object Conversion Values', () => {
   const jsonToCsvConversion = new JsonToCsvConversion(testJson);
-  expect(jsonToCsvConversion.values).toStrictEqual([
-    'John',
-    'Smith',
-    'Mr',
-    'HR',
-    'HR Assistant'
-  ]);
+  expect(jsonToCsvConversion.values).toStrictEqual(['John', 'Smith', 'Mr', 'HR', 'HR Assistant']);
 });
