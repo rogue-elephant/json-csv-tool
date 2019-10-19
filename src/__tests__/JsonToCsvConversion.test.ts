5c060b9c90af4b6a08f23e10fe42765a783f420b
DRandell <jup.trandell@gmail.com>
2019-10-19 12:41:17 +0100

    Moved exposed items to exports in index file

--- src/__tests__/JsonToCsvConversion.test.ts
+++ src/__tests__/JsonToCsvConversion.test.ts
@@ -1,27 +1,12 @@
-<<<<<<< HEAD
 import { JsonCsvConverter } from '../converter';
 import { SimplePersonJson } from '../_dummy-json/simple-person';
 
 test('Individual Object Conversion Column Names', () => {
   const jsonToCsvConversion = new JsonCsvConverter().convertJsonToCsv(SimplePersonJson[0]);
-=======
-import { JsonToCsvConversion } from '../conversion';
-const testJson = {
-  firstName: 'John',
-  lastName: 'Smith',
-  title: 'Mr',
-  department: 'HR',
-  job: 'HR Assistant',
-};
-
-test('Individual Object Conversion Column Names', () => {
-  const jsonToCsvConversion = new JsonToCsvConversion(testJson);
->>>>>>> bce7ecb225039b913f875ca56232fb2b4ce690f6
   expect(jsonToCsvConversion.columnNames).toStrictEqual(['firstName', 'lastName', 'title', 'department', 'job']);
 });
 
 test('Individual Object Conversion Values', () => {
-<<<<<<< HEAD
   const jsonToCsvConversion = new JsonCsvConverter().convertJsonToCsv(SimplePersonJson[0]);
   expect(jsonToCsvConversion.values).toStrictEqual([['John', 'Smith', 'Mr', 'HR', 'HR Assistant']]);
 });
@@ -38,8 +23,4 @@ test('Individual Object BlackList', () => {
     blackList: ['firstName', 'lastName'],
   });
   expect(jsonToCsvConversion.values).toStrictEqual([['Mr', 'HR', 'HR Assistant']]);
-=======
-  const jsonToCsvConversion = new JsonToCsvConversion(testJson);
-  expect(jsonToCsvConversion.values).toStrictEqual(['John', 'Smith', 'Mr', 'HR', 'HR Assistant']);
->>>>>>> bce7ecb225039b913f875ca56232fb2b4ce690f6
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import { SimplePersonJson } from './_dummy-json/simple-person';
-import { JsonCsvConverter } from './converter';
-
-const result = new JsonCsvConverter().convertJsonToCsv(SimplePersonJson);
-const csvOutput = result.csv;
+export { ConvertedCsv } from './models/converted-csv';
+export { IJsonToCsvConversionStrategy } from './models/json-to-csv-conversion-strategy';
+export { JsonCsvConverter } from './converter';
