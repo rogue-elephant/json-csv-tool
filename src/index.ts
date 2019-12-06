25baf795e7bc196ce0f200a1791014746e6b4c32
DRandell <jup.trandell@gmail.com>
2019-12-06 15:34:43 +0000

    changed > to . notation and got .* working as well as collapsing empty rows

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+import { JsonCsvConverter } from './converter';
+import { DeeperPersonJson } from './_dummy-json/deeper-person';
+import { trelloJson } from './_dummy-json/sensitive/trello';
+
 export { Table } from './models/table';
 export { IJsonToCsvConversionStrategy } from './models/json-to-csv-conversion-strategy';
 export { JsonCsvConverter } from './converter';
+
+const result = new JsonCsvConverter().convertJsonToCsv(trelloJson, {whiteList: ['options.value']});
+const result2 = new JsonCsvConverter().convertJsonToCsv(trelloJson, {whiteList: ['customFields.options.value']});
+const blah = result.csv;
