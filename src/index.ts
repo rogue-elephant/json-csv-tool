3b14a0c8991fc3edab98daa50fd5b1c8bc99ad99
DRandell <jup.trandell@gmail.com>
2019-12-08 11:57:25 +0000

    added markdown output and fixed bug with new line chars in values

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+import { JsonCsvConverter } from './converter';
+import { DeeperPersonJson } from './_dummy-json/deeper-person';
+import { trelloJson } from './_dummy-json/sensitive/trello';
+
 export { Table } from './models/table';
 export { IJsonToCsvConversionStrategy } from './models/json-to-csv-conversion-strategy';
 export { JsonCsvConverter } from './converter';
+
+const result = new JsonCsvConverter().convertJsonToCsv(trelloJson, {whiteList: ['name', 'desc', 'cards.name', 'cards.desc']});
+const foo = result.getMarkdown();
+const blah = result.getCsv();
+const poop = '';
--- src/models/table.ts
+++ src/models/table.ts
@@ -3,35 +3,99 @@ export class Table {
   public rows: IRowValue[][] = [];
   public title?: string;
 
-  public get csv(): string {
-    let output = '';
-    const outputTable = (table: Table) => {
-      output += this.columnNames.join(',') + '\r\n';
+  protected newlineChars = [
+    '\r',
+    '\n',
+    '\r\n',
+    '\n\n',
+    '\r\r',
+    '\u{000a}',
+    '\u{000b}',
+    '\u{000c}',
+    '\u{000d}',
+    '\u{0085}',
+    '\u{2028}',
+    '\u{2029}',
+  ];
 
-      output += this.rows
-        .map(row =>
-          this.columnNames
-            .map(columnName => {
-              const foundRowValues = row.filter(x => x.columnName === columnName);
-              return foundRowValues.length > 0 ? foundRowValues[0].value : '';
-            })
-            .join(','),
-        )
-        .join('\r\n');
-    };
-    outputTable(this);
-    this.rows
-      .filter(row => row.filter(x => x.linkedTable != null).length > 0)
-      .map(row => row.filter(x => x.linkedTable != null).map(x => x.linkedTable))
-      .forEach(tableList => {
-        tableList.forEach(table => {
-          table = table as Table;
-          output += `\r\n\r\n${table.title}\r\n${table.csv}`;
-        });
-      });
+  public getCsv(): string {
+    const removeUnwantedCharsPfa = (value: string) => this.removeUnwantedChars(value, [...this.newlineChars, ',']);
+    return this.generateOutput(
+      ',',
+      (output: string, table: Table) =>
+        output + `${table.title}\r\n` + table.columnNames.map(x => removeUnwantedCharsPfa(x)).join(',') + '\r\n',
+      (rowCol: IRowValue) => removeUnwantedCharsPfa(rowCol.value),
+    );
+  }
+
+  public getMarkdown(): string {
+    const removeUnwantedCharsPfa = (value: string) => this.removeUnwantedChars(value, this.newlineChars);
+    return this.generateOutput(
+      '|',
+      (output: string, table: Table) =>
+        output +
+        `# ${table.title} <a name="${(table.title as string).split(' ').join('_')}"></a>\r\n` +
+        '|' +
+        table.columnNames.map(x => '*' + removeUnwantedCharsPfa(x) + '*').join('|') +
+        '\r\n' +
+        '|' +
+        table.columnNames.map(x => '---').join('|') +
+        '\r\n',
+      (rowCol: IRowValue) => removeUnwantedCharsPfa(rowCol.value),
+      (rowCol: IRowValue) =>
+        `[${removeUnwantedCharsPfa(rowCol.value)}](#${((rowCol.linkedTable as Table).title as string)
+          .split(' ')
+          .join('_')})`,
+    );
+  }
+
+  public generateOutput(
+    seperator: string,
+    topLevelCallback?: any,
+    rowLevelCallback?: any,
+    rowLevelLinkedTableCallback?: any,
+  ): string {
+    let output = '';
+    output = topLevelCallback ? topLevelCallback(output, this) : output;
+    const linkedTables: Table[] = [];
+
+    output += this.rows
+      .map(row =>
+        this.columnNames
+          .map(columnName => {
+            const foundRowValues = row.filter(x => x.columnName === columnName);
+            if (foundRowValues.length > 0) {
+              if (foundRowValues[0].linkedTable != null) {
+                linkedTables.push(foundRowValues[0].linkedTable);
+                return rowLevelLinkedTableCallback
+                  ? rowLevelLinkedTableCallback(foundRowValues[0])
+                  : rowLevelCallback
+                  ? rowLevelCallback(foundRowValues[0])
+                  : foundRowValues[0].value;
+              }
+              return rowLevelCallback ? rowLevelCallback(foundRowValues[0]) : foundRowValues[0].value;
+            } else {
+              return '';
+            }
+          })
+          .join(seperator),
+      )
+      .join('\r\n');
+    linkedTables.forEach(
+      table =>
+        (output += `\r\n\r\n${table.generateOutput(
+          seperator,
+          topLevelCallback,
+          rowLevelCallback,
+          rowLevelLinkedTableCallback,
+        )}`),
+    );
 
     return output;
   }
+
+  protected removeUnwantedChars = (value: any, unwantedChars: string[], replaceWith: string = '') =>
+    value.toString().replace(new RegExp(unwantedChars.join('|'), 'g'), replaceWith);
 }
 
 export interface IRowValue {
